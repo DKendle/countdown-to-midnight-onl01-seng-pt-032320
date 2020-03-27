00ce19e68de18f70b1980677509aedeed659d154
Dina Kendle <dina.kendle@gmail.com>
2020-03-27 06:35:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+def countdown(number)
+  
+  while number > 0
+    puts "#{number} SECONDS(S)!"
+    number -= 1
+  end
+    puts "HAPPY NEW YEAR"
+end
 
-def countdown
+def countdown_with_sleep
+    while number > 0
+      puts "#{number} SECONDS(S)"
+      number -= 1
+      sleep(60)
+    end
+      puts "HAPPY NEW YEAR"
 end
